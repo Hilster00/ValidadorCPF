f6f1e2b02b78b8b726571bc57c735cf3e7d436a1
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2022-05-14 21:07:45 -0300

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,11 @@
+from ValidarCpf import validar_cpf 
+
+#percorrer todo o intervalo de cpf 000.000.000-00 a 999.999.999-99
+print('Lista de cpf válidos')
+quantidade=0
+for cpf in range(100000000000):
+    if validar_cpf(cpf) == True:
+        cpf_p=f'{cpf:0>11}'
+        print(f'{cpf_p[:3]}.{cpf_p[3:6]}.{cpf_p[6:9]}-{cpf_p[9:11]}')
+        quantidade+=1
+print(f'A quantidade de cpf validos são {quantidade}')
